b838a961a9112b5186998e2a6efbdfa7df93ef72
CarolainQuesada <carolain211222@gmail.com>
2024-08-25 10:28:53 -0600

    recovery: Vercion de recuperacion completa
    En esta vercion se encuentra toda la infomacion recuperada del proyecto,
    y la bse del mismo

--- NuevaCinema/NuevaCinema.cpp
+++ NuevaCinema/NuevaCinema.cpp
@@ -1,19 +1,55 @@
-// NuevaCinema.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
 #include <iostream>
+using namespace std;
+#include <string>
+#include"movie.h"
+#include"seat.h"
+#include"room.h"
+#include"booking.h"
+#include"sale.h"
+#include"schedule.h"
+#include"cinema.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Cinema cinema; // Crear una instancia del gestor de peliculas
+    int option;
+
+    do {
+        cout << "Menu Principal" << "\n" << endl;
+        cout << "1. Archivo" << endl;
+        cout << "2. Mantenimiento" << endl;
+        cout << "3. Reserva" << endl;
+        cout << "4. Venta" << endl;
+        cout << "5. Salir" << endl;
+        cout << "\nSelecciona una opcion:" << endl;
+        cin >> option;
+        system("cls");
+
+        switch (option) {
+        case 1:
+            cinema.menuFile();
+            system("cls");
+            break;
+        case 2:
+            cinema.maintenanceMenu();
+            system("cls");
+            break;
+        case 3:
+            cinema.reservationMenu();
+            system("cls");
+            break;
+        case 4:
+            cinema.saleMenu();
+            system("cls");
+            break;
+        case 5:
+            cout << "Saliendo..." << endl;
+            system("cls");
+            break;
+        default:
+            cout << "Opcion invalida!!" << "\n" << endl;
+        }
+    } while (option != 5);
+
+    return 0;
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
